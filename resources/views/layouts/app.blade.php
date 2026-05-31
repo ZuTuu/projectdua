0206b5c97574f3858c8da349449e00b566ca7e93
zutu <51734083+ZuTuu@users.noreply.github.com>
2026-05-31 18:11:44 +0700

    kjnas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    @vite(['resources/sass/app.scss', 'resources/css/style.css' 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/css/style.css', 'resources/js/app.js'])
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Paw Center - D&F Pet Shop</title>
-
+<!-- test hi -->
 </head>
 <body class="dashboard-body">
 
