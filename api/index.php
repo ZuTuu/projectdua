a2e1c9cd0f57c28a68b76a17401b727e44dd3908
zutu <51734083+ZuTuu@users.noreply.github.com>
2026-05-31 13:54:55 +0700

    brah

--- api/index.php
+++ api/index.php
@@ -1,7 +1,11 @@
 <?php
-// Paksa PHP menampilkan error ke layar browser
+// Paksa PHP nampilin error
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+// INI YANG LO SKIP BANG, PADAHAL INI KUNCINYA BIAR GAK CRASH!
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 
 require __DIR__ . '/../public/index.php';
