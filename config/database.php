5cfdf7ccb04bfd00663fd011c93cb7d1e6b8a2c5
zutu <51734083+ZuTuu@users.noreply.github.com>
2026-05-31 16:01:57 +0700

    buat cacert

--- config/database.php
+++ config/database.php
@@ -59,7 +59,11 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                // Mengaktifkan SSL
+                \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                
+                // TAMBAHKAN INI: Memaksa SSL aktif
+                \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, 
             ]) : [],
         ],
 
