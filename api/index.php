8748eb620358d615e812433e78910ea77126cd1c
zutu <51734083+ZuTuu@users.noreply.github.com>
2026-05-31 13:43:34 +0700

    public build

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+// Paksa PHP menampilkan error ke layar browser
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require __DIR__ . '/../public/index.php';
